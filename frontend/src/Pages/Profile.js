3b08303f75885f9ed9b8b50635d7cfc06b090865
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-01 10:14:42 -0400

    Add collection section to page

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -2,39 +2,58 @@ import { useEffect, useState } from "react"
 import NavBar from "../components/NavBar"
 import { useUser } from "../context/UserContext"
 import { getUser } from "../services/fetchers"
+import { useNavigate } from "react-router-dom"
+import BoardGameCard from "../components/BoardGameCard"
 
-function Profile(){
-  const { user, setUser } = useUser()
-  const [ userData, setUserData] = useState(null)
+function Profile() {
+  const { setUser } = useUser()
+  const [userData, setUserData] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchUser = async () => {
-      const savedUser = localStorage.getItem("userId");
-      const user = savedUser ? JSON.parse(savedUser) : null;
+      const savedUser = localStorage.getItem("userId")
+      const user = savedUser ? JSON.parse(savedUser) : null
       if (user) {
-        setUser(user);
-        const userResp = await getUser(user);
-        setUserData(userResp);
+        setUser(user)
+        const userResp = await getUser(user)
+        setUserData(userResp)
+      } else {
+        navigate("/")
       }
-    };
+    }
 
-    fetchUser();
-  }, [setUser]);
+    fetchUser()
+  }, [setUser])
 
-  if(!userData){
+  if (!userData) {
     return <h1>Loading...</h1>
   }
 
-  return(
+  const boardGameCollection = () => {
+    if(userData.board_games === "No board games"){
+      return <h3>No board games in collection</h3>
+    } else {
+      return userData.board_games.map((boardGame) => {
+        return <BoardGameCard key={boardGame.id} boardGame={boardGame} />
+      })
+    }
+  }
+
+  return (
     <div>
       <NavBar />
       <div className="profile">
         <h2 className="text-center">Profile</h2>
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Username: {userData.username}</h5>
+            <h5 className="card-title">Username: {userData.user.username}</h5>
           </div>
         </div>
+      </div>
+      <div>
+        <h2>Board Game Collection</h2>
+        {boardGameCollection()}
       </div>
     </div>
   )
