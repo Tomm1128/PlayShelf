3c7db1ad6a860d08b3fa90332cf4bee7f90d1e99
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-01 14:37:43 -0400

    Add collection endpoints for backend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   get "/users/:id" do
     user = User.find(params[:id])
     board_games = Collection.user_collection(params[:id])
-    { user: user, board_games: board_games }.to_json
+    { user: user, collection: board_games }.to_json
   end
 
   post "/users" do
@@ -44,5 +44,21 @@ class ApplicationController < Sinatra::Base
       password: params[:password]
     )
     user.to_json
+  end
+
+  post "/collections" do
+    collection = Collection.create(
+      user_id: params[:user_id]
+    )
+    { collection_id: collection.id, board_games: collection.board_games }.to_json
+  end
+
+  post "/collections/:id" do
+    CollectionBoardGame.create(
+      board_game_id: params[:board_game_id],
+      collection_id: params[:collection_id]
+    )
+    collection = Collection.find(params[:collection_id])
+    { collection_id: collection.id, board_games: collection.board_games }.to_json
   end
 end
